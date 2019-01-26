b3a99977a18ab2f5047afa94310066ea038858d9
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-01-26 11:02:36 +0100

    .

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.text.Font;
+import javafx.scene.control.RadioButton;
 import javafx.scene.text.Text;
 
 import java.util.Random;
@@ -14,27 +14,34 @@ public class Controller {
     @FXML Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
     @FXML Text turnoPlayer = new Text();
     @FXML Button start;
+    @FXML private RadioButton hvc, hvh, cvc;
 
     private char letra;
     private char turnotext;
-
     Random random = new Random();
-
     private boolean turno = random.nextBoolean();
 
 
-    @FXML public void clickButton(ActionEvent event) {
-        turno();
-        Button b = (Button) event.getSource();
-        b.setText(String.valueOf(letra));
+    public void clickButton(ActionEvent event) {
+
+       if (start.) {
+           if (hvh.isSelected()) {
+               hvh(event);
+
+           } else if (hvc.isSelected()) {
+               hvc(event);
+           }
+       }
     }
 
     public void start(){
 
-        turno();
+
+
+
     }
 
-    public boolean turno(){
+    public void hvh(ActionEvent event){
         if (turno == true) {
             letra ='X';
             turnotext = 'O';
@@ -46,7 +53,24 @@ public class Controller {
             turnoPlayer.setText("Es el turno de: "+ turnotext);
             turno = true;
         }
-        return true;
+        Button b = (Button) event.getSource();
+        b.setText(String.valueOf(letra));
+    }
+
+    public void hvc(ActionEvent event){
+        if (turno == true) {
+            letra ='X';
+            turnotext = 'O';
+            turnoPlayer.setText("Es el turno del ordenador: " + turnotext);
+            turno = false;
+        }else {
+            letra ='O';
+            turnotext = 'X';
+            turnoPlayer.setText("Es el turno del jugador: "+ turnotext);
+            turno = true;
+        }
+        Button b = (Button) event.getSource();
+        b.setText(String.valueOf(letra));
     }
 
 }
